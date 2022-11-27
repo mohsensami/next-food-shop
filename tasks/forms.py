b7cc43951f55551946098de8f873673550f70ef9
mohsensami <mohsensami88@gmail.com>
2022-11-27 13:52:40 +0330

    work on ui - by bootstrap

--- tasks/forms.py
+++ tasks/forms.py
@@ -5,7 +5,7 @@ from .models import *
 
 
 class TaskForm(forms.ModelForm):
-	title= forms.CharField(widget= forms.TextInput(attrs={'placeholder':'Add new task...'}))
+	title = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Add new task...'}))
 
 	class Meta:
 		model = Task
