1cf22690e33faba7552492953491fa21d7c893e3
mohsensami <mohsensami18727@gmail.com>
2024-04-30 17:43:28 +0330

    RestaurantList in home page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Image from 'next/image';
 import CategoryList from './_components/CategoryList';
+import BusinessList from './_components/BusinessList';
 
 export default function Home() {
     return (
         <div>
             <CategoryList />
+            <BusinessList />
         </div>
     );
 }
